8c07173074d85e93c719e8b72de3fe6208dcff8d
RosellAdan <rosell321adans@gmail.com>
2021-08-27 18:05:43 -0400

    continuando el proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HOME</title>
+</head>
+<body>
+  <h1>HOME</h1>
+    <header>
+        <nav>
+            <a href="<?php echo Route('home') ?>">Inicio</a>
+            <a href="<?php echo Route('saludos') ?>">Saludos</a>
+
+        </nav>
+    </header>
+</body>
+</html>
